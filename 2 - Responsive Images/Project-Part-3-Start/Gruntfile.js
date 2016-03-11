f4fe54a752de7bcbb33a143cda4c355fef453c0f
Sup-Dev <sourabh.dev90@gmail.com>
2016-03-11 23:38:03 +0530

    chore: updated grunt configuration

--- 2 - Responsive Images/Project-Part-3-Start/Gruntfile.js
+++ 2 - Responsive Images/Project-Part-3-Start/Gruntfile.js
@@ -18,6 +18,14 @@ module.exports = function(grunt) {
             width: 1600,
             suffix: '_large_2x',
             quality: 30
+          }, {
+            width: 800,
+            suffix: '_large_1x',
+            quality: 30
+          }, {
+            width: 500,
+            suffix: '_medium',
+            quality: 30
           }]
         },
 
