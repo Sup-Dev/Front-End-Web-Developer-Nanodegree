7b462535aaa87f716dad4f217458b704e7676719
Sup-Dev <sourabh.dev90@gmail.com>
2016-05-20 13:01:31 +0530

    feat: added NYTimes api

--- 5 - Intro To Ajax/minicourse-ajax-project/js/script.js
+++ 5 - Intro To Ajax/minicourse-ajax-project/js/script.js
@@ -20,6 +20,20 @@ function loadData() {
     console.log(streetURL);
     $body.append('<img class="bgimg" src="' + streetURL + '">');
 
+    // NYTimes
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "d7ddee8c903f4e2cbcb2474087b22b4c"
+});
+    $.getJSON(url, function(data) {
+      var docs = data.response.docs;
+      
+      for(var i=0; i<docs.length; i++) {
+        var item = '<li class="article"><a href="' + docs[i].web_url + '">' + docs[i].headline.main + "</a><p>" + docs[i].snippet + "</p></li>" ;
+        $('#nytimes-articles').append(item);
+      }
+    });
+  
     // YOUR CODE GOES HERE!
 
     return false;
