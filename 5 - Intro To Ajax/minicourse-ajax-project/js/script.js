37347b21457efd91b3e2fbf36ce55d1da834cc9d
Sup-Dev <sourabh.dev90@gmail.com>
2016-05-20 14:36:37 +0530

    feat: added wikipedia api

--- 5 - Intro To Ajax/minicourse-ajax-project/js/script.js
+++ 5 - Intro To Ajax/minicourse-ajax-project/js/script.js
@@ -17,24 +17,43 @@ function loadData() {
     var city = form_data[1]['value'];
     
     var streetURL = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + street + ", " + city;
-    console.log(streetURL);
+    //console.log(streetURL);
     $body.append('<img class="bgimg" src="' + streetURL + '">');
 
     // NYTimes
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "d7ddee8c903f4e2cbcb2474087b22b4c"
-});
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
+    url += city;
+    url += '&sort=newest&api-key=' + "d7ddee8c903f4e2cbcb2474087b22b4c";
+
     $.getJSON(url, function(data) {
       var docs = data.response.docs;
       
+      $nytHeaderElem.text('New York Times Articles About ' + city);
+      
       for(var i=0; i<docs.length; i++) {
         var item = '<li class="article"><a href="' + docs[i].web_url + '">' + docs[i].headline.main + "</a><p>" + docs[i].snippet + "</p></li>" ;
-        $('#nytimes-articles').append(item);
+        $nytElem.append(item);
       }
+    })
+    .error(function() {
+      $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
   
-    // YOUR CODE GOES HERE!
+    // wiki
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + city + "&format=json&callback-wikiCallback";
+  
+    $.ajax(url, {
+      dataType: 'jsonp',
+      success: function(response) {
+        console.log(response);
+        var items = response[1];
+        
+        for (var i=0; i < items.length; i++) {
+          var url = 'http://en.wikipedia.org/wiki/' + items[i];
+          $wikiElem.append('<li><a href="' + url + '">' + items[i] + '</a></li>');
+        };
+      }
+    })
 
     return false;
 };
