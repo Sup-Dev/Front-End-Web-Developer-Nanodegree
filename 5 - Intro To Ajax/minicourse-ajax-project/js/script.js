c8d9a9c6f7a50870f5c3c46c19036097375c2a41
Sup-Dev <sourabh.dev90@gmail.com>
2016-05-20 11:09:51 +0530

    feat: added api to google street view

--- 5 - Intro To Ajax/minicourse-ajax-project/js/script.js
+++ 5 - Intro To Ajax/minicourse-ajax-project/js/script.js
@@ -12,6 +12,13 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var form_data = $('#form-container').serializeArray();
+    var street = form_data[0]['value'];
+    var city = form_data[1]['value'];
+    
+    var streetURL = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + street + ", " + city;
+    console.log(streetURL);
+    $body.append('<img class="bgimg" src="' + streetURL + '">');
 
     // YOUR CODE GOES HERE!
 
