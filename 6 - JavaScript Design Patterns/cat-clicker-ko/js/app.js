700165f905d913caf679fa3fb024381fa7722d26
Sup-Dev <sourabh.dev90@gmail.com>
2016-05-28 14:56:45 +0530

    feat: knockoutjs based cat clicker now has level computer variable

--- 6 - JavaScript Design Patterns/cat-clicker-ko/js/app.js
+++ 6 - JavaScript Design Patterns/cat-clicker-ko/js/app.js
@@ -1,11 +1,21 @@
 var ViewModel = function() {
   this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
+  this.name = ko.observable('Tabby');  
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
   }
+  
+  this.level = ko.computed(function() {
+    if (this.clickCount() >= 100) {
+      return 'Teen';
+    } else if (this.clickCount() >= 10) {
+      return 'Infant';
+    } else {
+      return 'Newborn';
+    }
+  }, this);
 }
 
 ko.applyBindings(new ViewModel());
