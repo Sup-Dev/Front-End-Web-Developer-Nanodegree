58bc67dfd0a404d13cdc5a3a89717c42b4611396
Sup-Dev <sourabh.dev90@gmail.com>
2016-05-28 16:26:48 +0530

    feat: knockoutjs based cat clicker now complete with cat selection

--- 6 - JavaScript Design Patterns/cat-clicker-ko/js/app.js
+++ 6 - JavaScript Design Patterns/cat-clicker-ko/js/app.js
@@ -1,8 +1,41 @@
-var Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');  
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.nicknames = ko.observableArray(['Prince', 'Touch']);
+var initialCats = [
+  {
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    nicknames: ['Prince', 'Touch']
+  },
+  {
+    clickCount: 0,
+    name: 'Tiger',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    nicknames: ['Tigger']
+  },
+  {
+    clickCount: 0,
+    name: 'Scaredy',
+    imgSrc: 'img/22252709_010df3379e_z.jpg',
+    nicknames: ['Casper']
+  },
+  {
+    clickCount: 0,
+    name: 'Shadow',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    nicknames: ['Shooby']
+  },
+  {
+    clickCount: 0,
+    name: 'Sleepy',
+    imgSrc: 'img/9648464288_2516b35537_z.jpg',
+    nicknames: ['ZZZZ']
+  }
+];
+
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);  
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.nicknames = ko.observableArray(data.nicknames);
   
   this.level = ko.computed(function() {
     if (this.clickCount() >= 200) {
@@ -20,12 +53,23 @@ var Cat = function() {
 }
 
 var ViewModel = function() {
+  var self = this;
   
-  this.currentCat = ko.observable(new Cat());
+  this.catList = ko.observable([]);
+  
+  initialCats.forEach(function(catItem) {
+    self.catList().push(new Cat(catItem));
+  });
+  
+  this.currentCat = ko.observable(this.catList()[0]);
   
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
   };
+  
+  this.changeCat = function(cat) {
+    self.currentCat(cat);
+  }
 }
 
 ko.applyBindings(new ViewModel());
