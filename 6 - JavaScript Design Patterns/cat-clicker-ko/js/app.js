254d29880c9dfb2716810bbe7799c4a57bb7ecc3
Sup-Dev <sourabh.dev90@gmail.com>
2016-05-28 15:26:06 +0530

    feat: knockoutjs based cat clicker now has it's model separated

--- 6 - JavaScript Design Patterns/cat-clicker-ko/js/app.js
+++ 6 - JavaScript Design Patterns/cat-clicker-ko/js/app.js
@@ -1,22 +1,31 @@
-var ViewModel = function() {
+var Cat = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');  
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.nicknames = ['Prince', 'Touch'];
-  
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-  }
+  this.nicknames = ko.observableArray(['Prince', 'Touch']);
   
   this.level = ko.computed(function() {
-    if (this.clickCount() >= 100) {
+    if (this.clickCount() >= 200) {
+      return 'Adult';
+    } else if (this.clickCount() >= 100) {
       return 'Teen';
+    } else if (this.clickCount() >= 50) {
+      return 'Child';
     } else if (this.clickCount() >= 10) {
       return 'Infant';
     } else {
       return 'Newborn';
     }
   }, this);
+}
+
+var ViewModel = function() {
+  
+  this.currentCat = ko.observable(new Cat());
+  
+  this.incrementCounter = function() {
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+  };
 }
 
 ko.applyBindings(new ViewModel());
