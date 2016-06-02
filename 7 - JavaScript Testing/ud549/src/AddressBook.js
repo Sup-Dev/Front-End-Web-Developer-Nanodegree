351c940d129b102dfffbfb603f737a3071c1d62f
Sup-Dev <sourabh.dev90@gmail.com>
2016-06-02 08:51:18 +0530

    test: added tests for adding and deleting contact

--- 7 - JavaScript Testing/ud549/src/AddressBook.js
+++ 7 - JavaScript Testing/ud549/src/AddressBook.js
@@ -9,3 +9,7 @@ AddressBook.prototype.addContact = function(contact) {
 AddressBook.prototype.getContact = function(contactId) {
   return this.contacts[contactId];
 }
+
+AddressBook.prototype.deleteContact = function(contactId) {
+  this.contacts.splice(contactId, 1);
+}
